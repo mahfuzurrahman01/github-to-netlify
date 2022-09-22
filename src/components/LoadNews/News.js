a311df6097239e8cce8c2847a759bbf66e656642
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-22 21:30:46 +0600

    hello

--- src/components/LoadNews/News.js
+++ src/components/LoadNews/News.js
@@ -5,7 +5,7 @@ import Display from '../Display/Display';
 const News = () => {
     const [news,setNews] = useState([]);
     useEffect(() => {
-        fetch("http://newsapi.org/v2/everything?q=keyword&apiKey=8a81e8256bba423a843898a61adb9432")
+        fetch("https://newsapi.org/v2/everything?q=keyword&apiKey=8a81e8256bba423a843898a61adb9432")
         .then(res => res.json())
         .then(data => setNews(data.articles))
     },[])
